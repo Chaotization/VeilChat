5a136131fd33431b3132f7ff1427c312ec65987b
Xin Jin <jinxin8295@gmail.com>
2024-04-24 14:12:54 -0400

    updated aws setting

--- data-server/seed.js
+++ data-server/seed.js
@@ -1,4 +1,4 @@
 import {createUser} from "./data/users.js";
 
-const user1 = await createUser("Mike", "River", "mike.river12344@example.com", ["chinese"], "+19293335817", "Abc12345678!", "");
+const user1 = await createUser("Mike", "River", "mike.river1234456@example.com", ["chinese"], "male", "06/12/2023","+19293335817", "Abc12345678!", "C:\\Users\\jinxi\\git_repos\\VeilChat\\data-server\\public\\images.jpg");
 console.log(user1);
