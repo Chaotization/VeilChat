cda51b36160d6903e5459a06c3d3cd75ad95bf42
Xin Jin <jinxin8295@gmail.com>
2024-04-24 13:49:43 -0400

    added gender and dob attribute to user data

--- data-server/seed.js
+++ data-server/seed.js
@@ -1,4 +1,4 @@
 import {createUser} from "./data/users.js";
 
-const user1 = await createUser("Mike", "River", "mike.river123@example.com", ["chinese"], "+19293335817", "Abc12345678!", "C:\\Users\\jinxi\\git_repos\\VeilChat\\data-server\\public\\images.jpg");
+const user1 = await createUser("Mike", "River", "mike.river12344@example.com", ["chinese"], "+19293335817", "Abc12345678!", "");
 console.log(user1);
--- data-server/validation.js
+++ data-server/validation.js
@@ -197,6 +197,24 @@ const exportedMethods = {
             }
             return normalizedLanguage;
         });
+    },
+    validateDateTime(inputDate) {
+        const dateRegex = /^(0[1-9]|1[0-2])\/(0[1-9]|[12][0-9]|3[01])\/(19[0-9]{2}|20[0-9]{2})$/;
+
+        if (typeof inputDate !== "string") {
+            throw "Date must be a string.";
+        }
+        inputDate = inputDate.trim();
+        if (!dateRegex.test(inputDate)) {
+            throw "Invalid date format. Must be in MM/DD/YYYY format.";
+        }
+        return inputDate;
+    },
+    validateGender(gender) {
+        if (!gender) throw "Gender is not provided";
+        gender = gender.trim().toLowerCase();
+        if (gender !== 'male' && gender !== 'female') throw "Invalid gender provided. Gender must be either 'male' or 'female'.";
+        return gender;
     }
 
 }
