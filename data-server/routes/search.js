4c32844762bb7842f9dec7ed71f0d2763f5f3f73
Vivaswanth <kashyap.vivaswanth@gmail.com>
2024-05-07 12:54:44 -0400

    search updated

--- data-server/routes/search.js
+++ data-server/routes/search.js
@@ -1,46 +1,64 @@
 import { Router } from "express";
-import {searchData} from "../data/index.js";
+import { searchData } from "../data/index.js";
 const router = Router();
+const chatRooms = {};
+
+const generateChatId = () => {
+	return "chatId_" + Date.now() + Math.round(Math.random(0, 10) * 10);
+};
 
 router.route("/").post(async (req, res) => {
-    let uId = req.body.uId;
-    let gender, language, age, distance, position;
-    if(req.body.gender){
-        gender = req.body.gender;
-    }
-    if(req.body.language){
-        language = req.body.language;
-    }
-    if(req.body.age){
-        age = req.body.age;
-    }
-    if(req.body.distance){
-        position = req.body.userLocation;
-        distance = req.body.distance;
-    }
+	let uId = req.body.uId;
+	let gender, language, age, distance, position;
+	if (req.body.gender) {
+		gender = req.body.gender;
+	}
+	if (req.body.language) {
+		language = req.body.language;
+	}
+	if (req.body.age) {
+		age = req.body.age;
+	}
+	if (req.body.distance) {
+		position = req.body.userLocation;
+		distance = req.body.distance;
+	}
 
-    try {
-        let {userFound, selectedUser} = await searchData.filtering(uId,  {gender, language, age , distance, position});
-        //return the matched user's id, gender, language, age, distance, position
-        if(userFound){
-            return res.status(200).json({
-                success: true,
-                filteredUserId:selectedUser.uId
-            })
-        }else{
-            return res.status(404).json( {
-                success: false,
-                filteredUserId: null
-            })
-        }
-    } catch (error) {
-        console.error("Error:", error.message);
-        return res.status(500).json({
-            success: false,
-            message: 'Internal server error while filtering users'
-        });
-    }
+	try {
+		let { userFound, selectedUser } = await searchData.filtering(uId, {
+			gender,
+			language,
+			age,
+			distance,
+			position,
+		});
+
+		if (userFound) {
+			let chatId;
+			const chatKey = [uId, selectedUser.uId].sort().join("_");
+
+			if (chatRooms[chatKey]) {
+				chatId = chatRooms[chatKey];
+			} else {
+				chatId = generateChatId();
+				chatRooms[chatKey] = chatId;
+			}
+
+			return res.status(200).json({
+				success: true,
+				filteredUserId: selectedUser.uId,
+				chatId,
+			});
+		} else {
+			return res.status(404).json({
+				success: false,
+				filteredUserId: null,
+			});
+		}
+	} catch (error) {
+		console.error("Error:", error.message);
+		res.status(500).send("Internal Server Error");
+	}
 });
-
 
 export default router;
